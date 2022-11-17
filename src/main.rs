8e56b5f25da1a875e136a3ce8b9fc80d376dd4b2
Helly Guo <hellyguo@foxmail.com>
2022-11-17 17:21:10 +0800

    main structure

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,57 @@
+use std::env;
+use std::fs::File;
+use std::process;
+
+use log::info;
+use log::warn;
+use simplelog::*;
+
 fn main() {
-    println!("Hello, world!");
+    init_logger();
+    let args: Vec<String> = env::args().collect();
+    let args_len: u8 = args.len() as u8 - 1;
+    if args_len == 2 {
+        choose_parser(&args[1], &args[2]);
+    } else {
+        warn!("just need two arguments, got {} argument(s)", args_len);
+        process::exit(-1);
+    }
+}
+
+fn init_logger() {
+    CombinedLogger::init(vec![
+        TermLogger::new(
+            LevelFilter::Info,
+            Config::default(),
+            TerminalMode::Mixed,
+            ColorChoice::Auto,
+        ),
+        WriteLogger::new(
+            LevelFilter::Info,
+            Config::default(),
+            File::create("/tmp/tosql.log").unwrap(),
+        ),
+    ])
+    .unwrap();
+}
+
+fn choose_parser(mode: &String, dir: &String) {
+    if mode == "ibatis" {
+        info!("try to parse ibatis sqlmap files in {:?}", dir);
+        parse_ibatis(dir);
+    } else if mode == "mybatis" {
+        info!("try to parse mybatis mapper files in {:?}", dir);
+        parse_mybatis(dir);
+    } else {
+        warn!("not supported: {:?}", mode);
+        process::exit(-1);
+    }
+}
+
+fn parse_ibatis(dir: &String) {
+    info!("{:?}", dir);
+}
+
+fn parse_mybatis(dir: &String) {
+    info!("{:?}", dir);
 }
