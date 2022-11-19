c9c09ad4cb2e07a82b94c88706567d23eeb3a13b
Helly Guo <hellyguo@foxmail.com>
2022-11-19 11:02:55 +0800

    extract common method

--- src/parser/abt_parser.rs
+++ src/parser/abt_parser.rs
@@ -1,11 +1,37 @@
+use super::parse_helper;
 use log::*;
 use regex::Regex;
+use rstring_builder::StringBuilder;
+use std::collections::HashMap;
 use std::fs::File;
 use std::io::Write;
 use std::process;
 use std::*;
+use xml::attribute::*;
+use xml::name::*;
+use xml::reader::*;
 
 const CRLF: [u8; 1] = [0x0a];
+
+pub struct XmlParsedState {
+    pub in_statement: bool,
+    pub in_sql: bool,
+    pub sql_idx: i32,
+    pub include_temp_sqls: HashMap<i32, String>,
+    pub include_temp_sqls_ids: HashMap<String, i32>,
+}
+
+impl XmlParsedState {
+    pub fn new() -> Self {
+        return XmlParsedState {
+            in_statement: false,
+            in_sql: false,
+            sql_idx: 0,
+            include_temp_sqls: HashMap::new(),
+            include_temp_sqls_ids: HashMap::new(),
+        };
+    }
+}
 
 pub trait Parser {
     fn parse(&self, output_dir: &String, files: &Vec<String>) {
@@ -44,10 +70,84 @@ pub trait Parser {
         self.read_xml(file, sql_store);
     }
 
-    fn read_xml(&self, file: &String, sql_store: &mut Vec<String>);
+    fn read_xml(&self, filename: &String, sql_store: &mut Vec<String>) {
+        sql_store.push("-- ".to_string() + filename);
+        let file = fs::File::open(filename).unwrap();
+        let buf = io::BufReader::new(file);
+        let parser = EventReader::new(buf);
+        let mut builder = StringBuilder::new();
+        let mut state = XmlParsedState::new();
+        for e in parser {
+            match e {
+                Ok(XmlEvent::StartElement {
+                    name, attributes, ..
+                }) => {
+                    self.parse_start_element(name, attributes, &mut builder, &mut state, sql_store);
+                }
+                Ok(XmlEvent::EndElement { name }) => {
+                    self.parse_end_element(name, &mut builder, &mut state, sql_store);
+                }
+                Ok(XmlEvent::CData(s)) => {
+                    if state.in_statement || state.in_sql {
+                        builder.append(s);
+                    }
+                }
+                Ok(XmlEvent::Characters(s)) => {
+                    if state.in_statement || state.in_sql {
+                        builder.append(s);
+                    }
+                }
+                Err(e) => {
+                    info!("Error: {}", e);
+                    break;
+                }
+                _ => {}
+            }
+        }
+    }
+
+    fn parse_start_element(
+        &self,
+        name: OwnedName,
+        attributes: Vec<OwnedAttribute>,
+        builder: &mut StringBuilder,
+        state: &mut XmlParsedState,
+        sql_store: &mut Vec<String>,
+    );
+
+    fn parse_end_element(
+        &self,
+        name: OwnedName,
+        builder: &mut StringBuilder,
+        state: &mut XmlParsedState,
+        sql_store: &mut Vec<String>,
+    ) {
+        let element_name = name.local_name.as_str().to_ascii_lowercase();
+        if parse_helper::match_statement(&element_name) {
+            let sql = parse_helper::replace_included_sql(
+                builder,
+                &state.include_temp_sqls,
+                &state.include_temp_sqls_ids,
+            );
+            self.clear_and_push(&sql, sql_store);
+            state.in_statement = false;
+        } else if element_name == "sql" {
+            state
+                .include_temp_sqls
+                .insert(state.sql_idx, builder.to_string());
+            state.sql_idx += 1;
+            builder.clear();
+            state.in_sql = false;
+        }
+    }
+    fn clear_and_push(&self, origin_sql: &String, sql_store: &mut Vec<String>);
 
     fn save(&self, output_dir: &String, sql_store: Vec<String>) {
-        info!("write to {:?}/resut.sql, size: {:?}", output_dir, sql_store.len());
+        info!(
+            "write to {:?}/resut.sql, size: {:?}",
+            output_dir,
+            sql_store.len()
+        );
         let r = File::create(output_dir.to_string() + "/result.sql");
         if r.is_err() {
             warn!("try to write sql to {:?} failed", output_dir);
--- src/parser/ibatis_parser.rs
+++ src/parser/ibatis_parser.rs
@@ -1,12 +1,10 @@
-use super::abt_parser::Parser;
+use super::abt_parser::*;
 use super::parse_helper;
 use lazy_static::*;
-use log::*;
 use regex::Regex;
 use rstring_builder::StringBuilder;
-use std::collections::HashMap;
-use std::*;
-use xml::reader::{EventReader, XmlEvent};
+use xml::attribute::*;
+use xml::name::*;
 
 const PARSER: IBatisParser = IBatisParser {};
 
@@ -24,101 +22,60 @@ impl Parser for IBatisParser {
         return self.detect_match_with_regex(file, &RE);
     }
 
-    fn read_xml(&self, filename: &String, sql_store: &mut Vec<String>) {
-        sql_store.push("-- ".to_string() + filename);
-        let file = fs::File::open(filename).unwrap();
-        let buf = io::BufReader::new(file);
-        let parser = EventReader::new(buf);
-        let mut in_statement: bool = false;
-        let mut in_sql: bool = false;
-        let mut sql_idx: i32 = 0;
-        let mut builder = StringBuilder::new();
-        let mut include_temp_sqls: HashMap<i32, String> = HashMap::new();
-        let mut include_temp_sqls_ids: HashMap<String, i32> = HashMap::new();
-        for e in parser {
-            match e {
-                Ok(XmlEvent::StartElement {
-                    name, attributes, ..
-                }) => {
-                    let element_name = name.local_name.as_str().to_ascii_lowercase();
-                    if parse_helper::match_statement(&element_name) {
-                        in_statement = true;
-                        parse_helper::search_matched_attr(&attributes, "id", |attr| {
-                            sql_store.push("-- ".to_string() + attr.value.as_str());
-                        });
-                    } else if in_statement && element_name == "where" {
-                        builder.append("where ");
-                    } else if in_statement && element_name == "include" {
-                        parse_helper::search_matched_attr(&attributes, "refid", |attr| {
-                            builder.append("__INCLUDE_ID_");
-                            builder.append(attr.value.as_str());
-                            builder.append("_END__");
-                        });
-                    } else if in_statement {
-                        parse_helper::search_matched_attr(&attributes, "prepend", |attr| {
-                            builder.append(attr.value.as_str());
-                        });
-                    } else if element_name == "sql" {
-                        in_sql = true;
-                        parse_helper::search_matched_attr(&attributes, "id", |attr| {
-                            include_temp_sqls_ids.insert(attr.value.as_str().to_string(), sql_idx);
-                        });
-                    }
-                }
-                Ok(XmlEvent::EndElement { name }) => {
-                    let element_name = name.local_name.as_str().to_ascii_lowercase();
-                    if parse_helper::match_statement(&element_name) {
-                        let sql = parse_helper::replace_included_sql(
-                            &mut builder,
-                            &include_temp_sqls,
-                            &include_temp_sqls_ids,
-                        );
-                        clear_and_push(&sql, sql_store);
-                        in_statement = false;
-                    } else if element_name == "sql" {
-                        include_temp_sqls.insert(sql_idx, builder.to_string());
-                        sql_idx += 1;
-                        builder.clear();
-                        in_sql = false;
-                    }
-                }
-                Ok(XmlEvent::CData(s)) => {
-                    if in_statement || in_sql {
-                        builder.append(s);
-                    }
-                }
-                Ok(XmlEvent::Characters(s)) => {
-                    if in_statement || in_sql {
-                        builder.append(s);
-                    }
-                }
-                Err(e) => {
-                    info!("Error: {}", e);
-                    break;
-                }
-                _ => {}
-            }
+    fn parse_start_element(
+        &self,
+        name: OwnedName,
+        attributes: Vec<OwnedAttribute>,
+        builder: &mut StringBuilder,
+        state: &mut XmlParsedState,
+        sql_store: &mut Vec<String>,
+    ) {
+        let element_name = name.local_name.as_str().to_ascii_lowercase();
+        if parse_helper::match_statement(&element_name) {
+            state.in_statement = true;
+            parse_helper::search_matched_attr(&attributes, "id", |attr| {
+                sql_store.push("-- ".to_string() + attr.value.as_str());
+            });
+        } else if state.in_statement && element_name == "where" {
+            builder.append("where ");
+        } else if state.in_statement && element_name == "include" {
+            parse_helper::search_matched_attr(&attributes, "refid", |attr| {
+                builder.append("__INCLUDE_ID_");
+                builder.append(attr.value.as_str());
+                builder.append("_END__");
+            });
+        } else if state.in_statement {
+            parse_helper::search_matched_attr(&attributes, "prepend", |attr| {
+                builder.append(attr.value.as_str());
+            });
+        } else if element_name == "sql" {
+            state.in_sql = true;
+            parse_helper::search_matched_attr(&attributes, "id", |attr| {
+                state
+                    .include_temp_sqls_ids
+                    .insert(attr.value.as_str().to_string(), state.sql_idx);
+            });
         }
     }
-}
 
-fn clear_and_push(origin_sql: &String, sql_store: &mut Vec<String>) {
-    lazy_static! {
-        static ref RE0: Regex = Regex::new("[\r\n\t ]+").unwrap();
-        static ref RE1: Regex = Regex::new("#[^#{]+#").unwrap();
-        static ref RE2: Regex = Regex::new("\\$[^${]+\\$").unwrap();
-        static ref RE3: Regex = Regex::new("\\$\\{[^${]+\\}").unwrap();
-        static ref RE_FIX1: Regex = Regex::new("WHERE[ ]+AND").unwrap();
-        static ref RE_FIX2: Regex = Regex::new("WHERE[ ]+OR").unwrap();
+    fn clear_and_push(&self, origin_sql: &String, sql_store: &mut Vec<String>) {
+        lazy_static! {
+            static ref RE0: Regex = Regex::new("[\r\n\t ]+").unwrap();
+            static ref RE1: Regex = Regex::new("#[^#{]+#").unwrap();
+            static ref RE2: Regex = Regex::new("\\$[^${]+\\$").unwrap();
+            static ref RE3: Regex = Regex::new("\\$\\{[^${]+\\}").unwrap();
+            static ref RE_FIX1: Regex = Regex::new("WHERE[ ]+AND").unwrap();
+            static ref RE_FIX2: Regex = Regex::new("WHERE[ ]+OR").unwrap();
+        }
+        let mut sql = String::from(origin_sql);
+        sql = RE0.replace_all(sql.as_str(), " ").to_string();
+        sql = RE1.replace_all(sql.as_str(), ":?").to_string();
+        sql = RE2.replace_all(sql.as_str(), ":?").to_string();
+        sql = RE3
+            .replace_all(sql.as_str(), "__REPLACE_SCHEMA__")
+            .to_string();
+        sql = RE_FIX1.replace_all(sql.as_str(), "WHERE").to_string();
+        sql = RE_FIX2.replace_all(sql.as_str(), "WHERE").to_string();
+        sql_store.push(sql + ";");
     }
-    let mut sql = String::from(origin_sql);
-    sql = RE0.replace_all(sql.as_str(), " ").to_string();
-    sql = RE1.replace_all(sql.as_str(), ":?").to_string();
-    sql = RE2.replace_all(sql.as_str(), ":?").to_string();
-    sql = RE3
-        .replace_all(sql.as_str(), "__REPLACE_SCHEMA__")
-        .to_string();
-    sql = RE_FIX1.replace_all(sql.as_str(), "WHERE").to_string();
-    sql = RE_FIX2.replace_all(sql.as_str(), "WHERE").to_string();
-    sql_store.push(sql + ";");
 }
