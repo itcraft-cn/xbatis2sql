e232743d0fbe44155d9ed490e6ecf4367fabd608
Helly Guo <hellyguo@foxmail.com>
2023-08-14 18:21:42 +0800

    test: update test data

--- test.zsh
+++ test.zsh
@@ -2,6 +2,8 @@
 
 target/debug/xbatis2sql -m -t Oracle -s . -o /tmp
 grep -F "__INCLUDE_" /tmp/result.sql
+mv /tmp/result.sql /tmp/m.result.sql
 target/debug/xbatis2sql -i -t Oracle -s . -o /tmp
 grep -F "__INCLUDE_" /tmp/result.sql
+mv /tmp/result.sql /tmp/i.result.sql
 
