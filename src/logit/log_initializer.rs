b376e3ce1d0a20886d8691660840d85c26212651
Helly Guo <hellyguo@foxmail.com>
2023-08-14 11:42:19 +0800

    build: change log crate to log4rs

--- src/logit/log_initializer.rs
+++ src/logit/log_initializer.rs
@@ -1,58 +1,42 @@
-use log::{info, LevelFilter};
-use simplelog::{ColorChoice, CombinedLogger, Config, TermLogger, TerminalMode, WriteLogger};
-use std::{env, env::consts, fs::File, process};
+use log::LevelFilter;
+use log4rs::{
+    append::console::ConsoleAppender,
+    config::{Appender, Root},
+    encode::pattern::PatternEncoder,
+    Config,
+};
+use std::{process, sync::Once};
+
+const LOG_FORMAT: &str = "[{l}] {m}{n}";
 
 /// 日志初始化，写入 `stdout`，并写入临时文件夹下 `xbatis2sql.log`
-pub fn init_logger() {
-    let tmp_dir = env::temp_dir()
-        .as_path()
-        .to_str()
-        .unwrap_or(".")
-        .to_string();
-    let log_file_name = tmp_dir + "/xbatis2sql.log";
-    let log_file = create_log_file(&log_file_name);
-    let rs = CombinedLogger::init(vec![
-        TermLogger::new(
-            LevelFilter::Info,
-            Config::default(),
-            TerminalMode::Mixed,
-            ColorChoice::Auto,
-        ),
-        WriteLogger::new(LevelFilter::Info, Config::default(), log_file),
-    ]);
+pub(crate) fn init_logger() {
+    static INIT: Once = Once::new();
+    INIT.call_once(|| init_log4rs());
+}
+
+fn init_log4rs() {
+    let stdout_appender = ConsoleAppender::builder()
+        .encoder(Box::new(PatternEncoder::new(LOG_FORMAT)))
+        .build();
+    let config = Config::builder()
+        .appender(Appender::builder().build("stdout", Box::new(stdout_appender)))
+        .build(Root::builder().appender("stdout").build(LevelFilter::Info))
+        .unwrap_or_else(|e| {
+            eprintln!("hit error: {}", e.to_string());
+            process::exit(-1);
+        });
+    let rs = log4rs::init_config(config);
     if rs.is_err() {
         eprintln!(
-            "init logger error: {}",
-            rs.err().unwrap_or_else(|| {
-                eprintln!("create log file[{}] error", log_file_name);
-                process::exit(-1);
-            })
+            "hit error: {}",
+            rs.err()
+                .unwrap_or_else(|| {
+                    eprintln!("hit error, failed to parse log4rs config then create the logger");
+                    process::exit(-1);
+                })
+                .to_string()
         );
         process::exit(-1);
     }
-    info!(
-        "log inited success, will output to stdout and {:?}",
-        log_file_name
-    );
-}
-
-fn create_log_file(log_file_name: &String) -> File {
-    let rs_log_file = File::create(log_file_name.clone());
-    rs_log_file.unwrap_or_else(|_e| {
-        eprintln!("create log file error: {}", &log_file_name);
-        let rs_log_file = match consts::OS {
-            "windows" => File::create("xbatis2sql.log"),
-            "linux" => File::create("/tmp/xbatis2sql.log"),
-            "macos" => File::create("/tmp/xbatis2sql.log"),
-            "unix" => File::create("/tmp/xbatis2sql.log"),
-            _ => {
-                eprintln!("Unknown OS: {}", consts::OS);
-                process::exit(-1);
-            }
-        };
-        rs_log_file.unwrap_or_else(|e| {
-            eprintln!("create log file[{}] error: {}", &log_file_name, e);
-            process::exit(-1);
-        })
-    })
 }
